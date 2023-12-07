2e3964b931a281b0fb3a80aa21d2fd8064cde43e
Vivien Rindisbacher <Vivien.Rindisbacher@dimensional.com>
2023-12-07 17:09:41 -0600

    Finish day 7

--- bin/day7/day7.rs
+++ bin/day7/day7.rs
@@ -23,6 +23,50 @@ enum Hand {
     HighCard(String, u64),
 }
 
+fn second_ordering_pt2(h1: String, h2: String) -> Ordering {
+    for (c1, c2) in h1.chars().zip(h2.chars()) {
+        match c1 {
+            'A' => match c2 {
+                'A' => (),
+                _ => return Ordering::Less,
+            },
+            'K' => match c2 {
+                'A' => return Ordering::Greater,
+                'K' => (),
+                _ => return Ordering::Less,
+            },
+            'Q' => match c2 {
+                'A' | 'K' => return Ordering::Greater,
+                'Q' => (),
+                _ => return Ordering::Less,
+            },
+            'T' => match c2 {
+                'A' | 'K' | 'Q' => return Ordering::Greater,
+                'T' => (),
+                _ => return Ordering::Less,
+            },
+            '9' | '8' | '7' | '6' | '5' | '4' | '3' | '2' => match c2 {
+                'A' | 'K' | 'Q' | 'T' => return Ordering::Greater,
+                _ => match c2 {
+                    'J' => return Ordering::Less,
+                    _ => match c2.cmp(&c1) {
+                        Ordering::Less => return Ordering::Less,
+                        Ordering::Equal => (),
+                        Ordering::Greater => return Ordering::Greater,
+                    },
+                },
+            },
+            'J' => match c2 {
+                'J' => (),
+                _ => return Ordering::Greater,
+            },
+            _ => (),
+        }
+    }
+    // doesn't matter
+    Ordering::Greater
+}
+
 fn second_ordering(h1: String, h2: String) -> Ordering {
     for (c1, c2) in h1.chars().zip(h2.chars()) {
         match c1 {
@@ -52,7 +96,7 @@ fn second_ordering(h1: String, h2: String) -> Ordering {
                 'T' => (),
                 _ => return Ordering::Less,
             },
-            '9' | '8' | '7' | '6' | '5' | '4' | '3' | '2' | '1' => match c2 {
+            '9' | '8' | '7' | '6' | '5' | '4' | '3' | '2' => match c2 {
                 'A' | 'K' | 'Q' | 'J' | 'T' => return Ordering::Greater,
                 _ => match c2.cmp(&c1) {
                     Ordering::Less => return Ordering::Less,
@@ -80,6 +124,7 @@ fn part2() {
                 .parse::<u64>()
                 .expect("Could not parse rank to int");
             let unique_cards = hand.chars().collect::<HashSet<char>>();
+            let num_jokers = hand.matches('J').count();
             match unique_cards.len() {
                 1 => Hand::FiveOfKind(hand.to_string(), rank),
                 2 => match unique_cards
@@ -88,8 +133,20 @@ fn part2() {
                     .max()
                     .expect("No max")
                 {
-                    4 => Hand::FourOfKind(hand.to_string(), rank),
-                    3 => Hand::FullHouse(hand.to_string(), rank),
+                    4 => {
+                        if num_jokers == 1 || num_jokers == 4 {
+                            Hand::FiveOfKind(hand.to_string(), rank)
+                        } else {
+                            Hand::FourOfKind(hand.to_string(), rank)
+                        }
+                    }
+                    3 => {
+                        if num_jokers == 2 || num_jokers == 3 {
+                            Hand::FiveOfKind(hand.to_string(), rank)
+                        } else {
+                            Hand::FullHouse(hand.to_string(), rank)
+                        }
+                    }
                     _ => panic!("No matching case for 2 unique cards"),
                 },
                 3 => match unique_cards
@@ -98,15 +155,106 @@ fn part2() {
                     .max()
                     .expect("No max")
                 {
-                    3 => Hand::ThreeOfKind(hand.to_string(), rank),
-                    2 => Hand::TwoPair(hand.to_string(), rank),
+                    3 => {
+                        if num_jokers == 1 || num_jokers == 3 {
+                            Hand::FourOfKind(hand.to_string(), rank)
+                        } else {
+                            Hand::ThreeOfKind(hand.to_string(), rank)
+                        }
+                    }
+                    2 => {
+                        if num_jokers == 2 {
+                            Hand::FourOfKind(hand.to_string(), rank)
+                        } else if num_jokers == 1 {
+                            Hand::FullHouse(hand.to_string(), rank)
+                        } else {
+                            Hand::TwoPair(hand.to_string(), rank)
+                        }
+                    }
                     _ => panic!("No matching case for 3 unique cards"),
                 },
-                4 => Hand::OnePair(hand.to_string(), rank),
-                5 => Hand::HighCard(hand.to_string(), rank),
+                4 => {
+                    if num_jokers == 1 || num_jokers == 2 {
+                        Hand::ThreeOfKind(hand.to_string(), rank)
+                    } else {
+                        Hand::OnePair(hand.to_string(), rank)
+                    }
+                }
+                5 => {
+                    if num_jokers == 1 {
+                        Hand::OnePair(hand.to_string(), rank)
+                    } else {
+                        Hand::HighCard(hand.to_string(), rank)
+                    }
+                }
                 _ => panic!("More than 5 unique cards"),
             }
         })
+        .collect::<Vec<Hand>>();
+    hands.sort_by(|a, b| match a {
+        Hand::FiveOfKind(h1, _) => match b {
+            Hand::FiveOfKind(h2, _) => second_ordering_pt2(h1.to_string(), h2.to_string()),
+            _ => std::cmp::Ordering::Less,
+        },
+        Hand::FourOfKind(h1, _) => match b {
+            Hand::FiveOfKind(_, _) => std::cmp::Ordering::Greater,
+            Hand::FourOfKind(h2, _) => second_ordering_pt2(h1.to_string(), h2.to_string()),
+            _ => std::cmp::Ordering::Less,
+        },
+        Hand::FullHouse(h1, _) => match b {
+            Hand::FiveOfKind(_, _) | Hand::FourOfKind(_, _) => std::cmp::Ordering::Greater,
+            Hand::FullHouse(h2, _) => second_ordering_pt2(h1.to_string(), h2.to_string()),
+            _ => std::cmp::Ordering::Less,
+        },
+        Hand::ThreeOfKind(h1, _) => match b {
+            Hand::FiveOfKind(_, _) | Hand::FourOfKind(_, _) | Hand::FullHouse(_, _) => {
+                std::cmp::Ordering::Greater
+            }
+            Hand::ThreeOfKind(h2, _) => second_ordering_pt2(h1.to_string(), h2.to_string()),
+            _ => std::cmp::Ordering::Less,
+        },
+        Hand::TwoPair(h1, _) => match b {
+            Hand::FiveOfKind(_, _)
+            | Hand::FourOfKind(_, _)
+            | Hand::FullHouse(_, _)
+            | Hand::ThreeOfKind(_, _) => std::cmp::Ordering::Greater,
+            Hand::TwoPair(h2, _) => second_ordering_pt2(h1.to_string(), h2.to_string()),
+            _ => std::cmp::Ordering::Less,
+        },
+        Hand::OnePair(h1, _) => match b {
+            Hand::FiveOfKind(_, _)
+            | Hand::FourOfKind(_, _)
+            | Hand::FullHouse(_, _)
+            | Hand::ThreeOfKind(_, _)
+            | Hand::TwoPair(_, _) => std::cmp::Ordering::Greater,
+            Hand::OnePair(h2, _) => second_ordering_pt2(h1.to_string(), h2.to_string()),
+            _ => std::cmp::Ordering::Less,
+        },
+        Hand::HighCard(h1, _) => match b {
+            Hand::FiveOfKind(_, _)
+            | Hand::FourOfKind(_, _)
+            | Hand::FullHouse(_, _)
+            | Hand::ThreeOfKind(_, _)
+            | Hand::TwoPair(_, _)
+            | Hand::OnePair(_, _) => std::cmp::Ordering::Greater,
+            Hand::HighCard(h2, _) => second_ordering_pt2(h1.to_string(), h2.to_string()),
+        },
+    });
+    let hands_len = hands.len();
+    let res = hands
+        .into_iter()
+        .enumerate()
+        .map(|(idx, h)| match h {
+            Hand::FiveOfKind(_, r)
+            | Hand::FourOfKind(_, r)
+            | Hand::FullHouse(_, r)
+            | Hand::ThreeOfKind(_, r)
+            | Hand::TwoPair(_, r)
+            | Hand::OnePair(_, r)
+            | Hand::HighCard(_, r) => (hands_len - idx) as u64 * r,
+        })
+        .sum::<u64>();
+    println!("{res}");
 }
 
 fn part1() {
@@ -199,7 +347,6 @@ fn part1() {
             | Hand::TwoPair(_, _)
             | Hand::OnePair(_, _) => std::cmp::Ordering::Greater,
             Hand::HighCard(h2, _) => second_ordering(h1.to_string(), h2.to_string()),
-            _ => std::cmp::Ordering::Less,
         },
     });
     let hands_len = hands.len();
@@ -221,4 +368,5 @@ fn part1() {
 
 fn main() {
     part1();
+    part2();
 }
